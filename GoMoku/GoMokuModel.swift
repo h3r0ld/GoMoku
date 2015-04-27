6c35086ddd1fb9945294251c54c5a6fe330212da
h3r0ld <kherold@sch.bme.hu>
2015-04-27 13:48:18 +0200

    Implemented checkForWinDiagonalWithDirLR

--- GoMoku/GoMokuModel.swift
+++ GoMoku/GoMokuModel.swift
@@ -15,7 +15,6 @@ class GoMokuModel {
         matrix = Array(count: size, repeatedValue: Array(count: size, repeatedValue: 0))
     }
     
-    
     func getMatrixAtIdx(XCoord x: Int,YCoord y:Int) -> Int {
         return matrix[x][y]
     }
@@ -32,17 +31,16 @@ class GoMokuModel {
         var sequenceCountHor: Int
         var sequenceCountVer: Int
         
-        for i in 0...matrix.count-1 {
+        for i in 0..<matrix.count {
             
             sequenceCountHor = 0
             sequenceCountVer = 0
             
-            for j in 0...matrix[i].count-2 {
+            for j in 0..<matrix[i].count-1 {
                 
-                //if the right neighbour is the same - so it's a sequence
+                //if the "right" neighbour is the same - so it's a sequence horizontally
                 if matrix[i][j] != 0 && matrix[i][j] == matrix [i][j+1] {
                     sequenceCountHor++;
-                    
                     //sequenceSize -1 comparison needed to specify the sequenceSize long sequence
                     if sequenceCountHor == sequenceSize - 1{
                         return matrix[i][j]
@@ -50,7 +48,7 @@ class GoMokuModel {
                 } else {
                     sequenceCountHor = 0;
                 }
-                //if the "down" neighbour is the same
+                //if the "under" neighbour is the same - so it's a sequence vertically
                 if matrix[j][i] != 0 && matrix[j][i] == matrix [j+1][i] {
                     sequenceCountVer++;
                     
@@ -67,16 +65,63 @@ class GoMokuModel {
         return -1
     }
     
+    
     func checkForWinDiagonalWithDirLR(sequenceSize: Int) -> Int {
+        var sequenceCount = 0
+        
+        for var i = 0; i < matrix.count; i++ {
+            
+            if (i == 0) {
+                
+                for var j = 0; j < matrix.count; j++ {
+                    var y = j;
+                    for var x = i; x < matrix.count - j; x++ {
+                        //                        myGoMoku.setMatrixAtIdx(XCoord: x, YCoord: y, Value: 1)
+                        if matrix[x][y] != 0 && matrix[x][y] == matrix[x+1][y+1] {
+                            println("UT: \(x) , \(y)")
+                            sequenceCount++
+                            if sequenceCount == sequenceSize - 1 {
+                                return matrix[x][y]
+                            }
+                            println("\(sequenceCount)")
+                        } else {
+                            sequenceCount = 0
+                        }
+                        y++
+                    }
+                }
+            } else {
+                var y = 0
+                for var x = i; x < matrix.count; x++ {
+                    //                    myGoMoku.setMatrixAtIdx(XCoord: x, YCoord: y, Value: 1)
+                    
+                    if matrix[x][y] != 0 && matrix[x][y] == matrix[x+1][y+1] {
+                        //                        println("UT: \(x) , \(y)")
+                        sequenceCount++
+                        if sequenceCount == sequenceSize - 1 {
+                            return matrix[x][y]
+                        }
+                        println("\(sequenceCount)")
+                    } else {
+                        sequenceCount = 0
+                    }
+                    y++
+                }
+            }
+            
+        }
+        
+        
         return -1
     }
+
     
     func checkForWinDiagonalWithDirRL(sequenceSize: Int) -> Int {
         return -1
     }
     
     func printMatrix() {
-        for i in 0...matrix.count-1 {
+        for i in 0..<matrix.count {
             for j in 0...matrix[i].count-1 {
                 print("\(matrix[i][j]) ")
             }
