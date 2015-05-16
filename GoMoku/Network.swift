ebb3b143edd8706c45b65023b5564cf9baf6b9cb
h3r0ld <kherold@sch.bme.hu>
2015-05-16 16:20:10 +0200

    Smoother gui

---
+++ GoMoku/Network.swift
@@ -0,0 +1,9 @@
+//
+//  Network.swift
+//  GoMoku
+//
+//  Created by Herold Kristóf on 04/05/15.
+//  Copyright (c) 2015 Herold Kristóf. All rights reserved.
+//
+
+import Foundation
---
+++ GoMoku/WinSequenceViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WinSequenceViewController.swift
+//  GoMoku
+//
+//  Created by Herold Kristóf on 16/05/15.
+//  Copyright (c) 2015 Herold Kristóf. All rights reserved.
+//
+
+import UIKit
+
+class WinSequenceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
